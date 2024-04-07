067db2a5448e1b5459cf00bd64319de8086d420c
H4mB3rg3r <153005235+H4mB3rg3r@users.noreply.github.com>
2024-04-07 20:59:28 +0800

    first project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,311 @@
+<template>
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+      <div class="head">
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center align-center">
+            <div style="position: relative; z-index: 9999;" class="mt-16 text-center">
+              <h1 class="text-grey">Hello,</h1>
+              <h1 class="text-white">I'm Joseph Carlo Tampus</h1>
+              <span class="text-grey">Web Designer & Developer</span><br />
+
+              <v-btn tile dark class="text-yellow mt-8" variant="outlined">
+                Contact me
+              </v-btn>
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div
+              style="
+                position: absolute;
+                z-index: 9999;
+                bottom: 0;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;
+              "
+              class="mt-16"
+            >
+              <v-icon>fas fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+      <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="i2.png" max-height="300" class=""></v-img>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16">About Me</h5>
+              <div style="width: 120px">
+                <v-slider
+                  v-model="slider2"
+                  color="black"
+                  label="track-color"
+                ></v-slider>
+              </div>
+              <h4 class="mt-n4">I Am a Creative Web Designer And Developer</h4>
+              <h4 class=""> <br /></h4>
+              <p class="text-grey">
+                I'm currently a student in Cordova Public College, passionately honing my skills
+                and developmet. My journey into the world of technology began with curiosity 
+                for crafting engaging and user-friendly digital experiences.
+              </p>
+              <br />
+              <br />
+              <h4 class="mt-n4">Aspiring Web Creative Designer & Developer</h4>
+              <p class="text-grey">
+                Driven by a desire to innovate and create, I am on a mission to blend 
+                aesthetics with functionality. My goal is to leverge cutting-edge 
+                technologies to build immersive websites that leave a lasting impression.
+              </p>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+      <div class="text-center mt-4">
+        <h2>Technologies I've Learned</h2>
+      </div>
+      <v-col cols="12" class="padd" id="portfolio">
+        <div class="first" id="project">
+          <v-row>
+            <v-col cols="12">
+              <div class="child">
+          
+                <h3 class="ml-3 mt-4">VUE</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Vue uses a simple and intuitive syntax to <br />
+                   declare the structure of your UI, <br />
+                   making it easy to understand and maintain. 
+                </p>
+              </div>
+              <div class="child">
+                <h3 class="ml-3 mt-4">HTML</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  
+                  HTML (HyperText Markup Language) <br />
+                  is the standard markup language used to create and 
+                  <br />structure content on the web.
+                </p>
+              </div>
+              <div class="child">
+                
+                <h3 class="ml-3 mt-4">CSS</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  
+                    CSS (Cascading Style Sheets) is a stylesheet language <br />  
+                    used to style and format the visual presentation of <br/> HTML and XML documents on the web.
+                  <br />
+                </p>
+              </div>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" id="services">
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="#FBDF7E" class="mr-2">My Photos</v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" class="imgHover">
+        <v-row class="fill-height" align="center" justify="center">
+          <template v-for="(item, i) in items" :key="i">
+            <v-col cols="12" md="4">
+              <v-hover v-slot="{ isHovering, props }">
+                <v-card
+                  :elevation="isHovering ? 12 : 2"
+                  :class="{ 'on-hover': isHovering }"
+                  v-bind="props"
+                >
+                  <v-img :src="item.img" height="225px" cover> </v-img>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </template>
+        </v-row>
+      </v-col>
+     <br /><br />
+      <v-col cols="12" sm="12" class="px-16" id="contact">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <div class="child">
+              <h1>Contact info.</h1>
+              <v-btn
+                icon="fas fa-map-marker-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">Poblacion, Cordova, Cebu </span
+              ><br />
+              <v-btn
+                icon="fas fa-phone-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">09156672653 </span> <br />
+              <v-btn
+                icon="fas fa-envelope"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">carlo.lendio22@gmail.com </span> <br />
+            </div>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <h1 class="mt-8">Send your message</h1>
+            <v-divider></v-divider>
+            <v-row class="mt-10">
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Name"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Phone No"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-textarea
+              label="Message"
+              persistent-hint
+              variant="outlined"
+            ></v-textarea>
+            <v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-container>
+    <FooterView />
+  </v-app>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import NavBar from "../components/NavBar.vue";
+import FooterView from "../components/FooterView.vue";
+
+// Components
+//import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: "HomeView",
+  setup() {
+    return {
+      slider2: 50,
+
+      items: [
+        {
+          img: "i3.jpg",
+        },
+        {
+          img: "i4.jpg",
+        },
+        {
+          img: "i5.jpg",
+        },
+        {
+          img: "i6.jpg",
+        },
+        {
+          img: "i9.jpg",
+        },
+        {
+          img: "i8.jpg",
+        },
+      ],
+    };
+  },
+  components: {
+    NavBar,
+    FooterView
+},
+});
+</script>
+<style scoped>
+.v-container {
+  padding: 16px 0 16px 0;
+}
+
+.head {
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width: 100%;
+  color: white;
+}
+.head:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, 6deg);
+}
+.head:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 50%;
+  background: black;
+  transform: skew(0deg, -6deg);
+}
+.egg {
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #141413;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
+}
+.first {
+  width: 100%;
+  height: 280px;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: center;
+  margin-right: 8px;
+}
+.imgHover {
+  padding: 0 200px;
+}
+.pre {
+  width: 100%;
+  height: 380px;
+  text-align: center;
+  padding: 0 200px;
+  background-color: #f5f5f5;
+}
+
+
+</style>
